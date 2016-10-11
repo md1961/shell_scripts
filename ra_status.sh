deecd38e0f74b102c969877b74e0fd1e28a2a223
Naoyuki Kumagai <kumagai@namaka>
2016-10-11 11:32:03 +0900

    [ra_status] Move project name definition to external file

--- ra_status.sh
+++ ra_status.sh
@@ -1,13 +1,16 @@
 #! /bin/sh
 
-projects='grassline application_templates asagao-extended baukis-on-rails-4-2'
-home_projects='ruby bin'
-others='coldhat storm paiza_exercise itunes hells'
-other_home_projects='igo-html5'
+. $(dirname $0)/ra_status_vars
+# Define the following variables in the above file:
+#   projects='grassline application_templates asagao-extended baukis-on-rails-4-2'
+#   home_projects='ruby bin topaz'
+#   others='coldhat storm itunes hells'
+#   other_home_projects='igo-html5 paiza_exercise'
 
 if [ "$1" = "--all" -o "$1" = "-a" ]
 then
 	projects="$projects $others"
+	home_projects="$home_projects $other_home_projects"
 elif [ "$1" = "-o" ]
 then
 	projects=$others
