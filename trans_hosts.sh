3e627a29287e80b8116d0552809846fe42ff739c
Naoyuki Kumagai <kumagai@namaka>
2017-08-04 08:57:53 +0900

    [trans_hosts] Correct by using "$*" instead of "$@" for localhost command

--- trans_hosts.sh
+++ trans_hosts.sh
@@ -7,9 +7,9 @@ HOSTS=$(cat $HOSTS_FILE)
 for host in $HOSTS
 do
 	echo -e "\e[36m===> $host <===\e[m"
-	ssh $host "source ~/.bash_profile; $@"
+	ssh $host "source ~/.bash_profile; $*"
 	echo
 done
 
 echo -e "\e[36m===> `hostname` <===\e[m"
-bash -c "$@"
+bash -c "$*"
