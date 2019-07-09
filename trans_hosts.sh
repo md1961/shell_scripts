7894c6d2ba6cda33d4e5d1175bf9be04b3258e87
Naoyuki Kumagai <kumagai@{{ remote_hostname }}>
2019-07-10 08:13:03 +0900

    [trans_hosts] Correct to 'set -e' for the whole and unset while ssh

--- trans_hosts.sh
+++ trans_hosts.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -Cu
+set -Ceu
 
 TIMEOUT_IN_SECOND=3
 
@@ -8,9 +8,11 @@ HOSTS=$(cat $HOSTS_FILE)
 
 for host in $HOSTS
 do
-	echo -e "\e[36m===> $host <===\e[m"
-	ssh -o "ConnectTimeout $TIMEOUT_IN_SECOND" $host "source ~/.bash_profile; $*"
-	echo
+    echo -e "\e[36m===> $host <===\e[m"
+    set +e
+    ssh -o "ConnectTimeout $TIMEOUT_IN_SECOND" $host "source ~/.bash_profile; $*"
+    set -e
+    echo
 done
 
 echo -e "\e[36m===> `hostname` <===\e[m"
