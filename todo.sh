287ed8d0febe9404ec12ca2566b093b5cbe4aa3b
Naoyuki Kumagai <kumagai@{{ remote_hostname }}>
2019-07-31 14:47:30 +0900

    [todo] Quit using trans_hosts.sh, and add edit mode

--- todo.sh
+++ todo.sh
@@ -1,3 +1,28 @@
 #! /bin/sh
+set -Ceu
 
-trans_hosts.sh 'cat ~/todo.txt 2> /dev/null'
+HOST=rael
+FILENAME=~/todo.txt
+
+EDIT=
+if [ $# -eq 1 ] && [ "$1" = "-e" ]
+then
+    EDIT=1
+fi
+
+if [ "$(hostname)" = "$HOST" ]
+then
+    if [ $EDIT ]
+    then
+        vim $FILENAME
+    else
+        cat $FILENAME
+    fi
+else
+    if [ $EDIT ]
+    then
+        vim scp://$HOST/$FILENAME
+    else
+        ssh $HOST cat $FILENAME
+    fi
+fi
